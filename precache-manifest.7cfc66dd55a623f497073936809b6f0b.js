9b9b1e2a27ba26f9c4bab6199d608f9baa1c08ed
PriyankDeep <priyank deep singh>
2020-06-20 19:32:27 +0530

    Updates

---
+++ precache-manifest.7cfc66dd55a623f497073936809b6f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5dc2373153457b30b1c917a40546f2",
+    "url": "/React-ToDo-App.git/index.html"
+  },
+  {
+    "revision": "07bb85d52e0002447866",
+    "url": "/React-ToDo-App.git/static/css/main.1933d66f.chunk.css"
+  },
+  {
+    "revision": "a7baa2360f11773f5d41",
+    "url": "/React-ToDo-App.git/static/js/2.cfa020ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-App.git/static/js/2.cfa020ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bb85d52e0002447866",
+    "url": "/React-ToDo-App.git/static/js/main.9ee5ffc5.chunk.js"
+  },
+  {
+    "revision": "53d563ba204bcd66efb5",
+    "url": "/React-ToDo-App.git/static/js/runtime-main.0ca99533.js"
+  }
+]);
